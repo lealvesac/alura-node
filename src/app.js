d682b80611bf693dab3acafcca74226d80438703
lealvesac <lealvesac@gmail.com>
2023-05-16 14:55:33 -0300

    implementação-coenxao-db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import dataBase from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
 
 dataBase.on("error", console.log.bind(console, "Erro de conexão!"));
 dataBase.once("open", () => {
@@ -10,10 +11,10 @@ const app = express();
 
 app.use(express.json());
 
-const livros = [
-  { id: 1, titulo: "O Senhor dos Aneis." },
-  { id: 2, titulo: "O Hobbit." },
-];
+// const livros = [
+//   { id: 1, titulo: "O Senhor dos Aneis." },
+//   { id: 2, titulo: "O Hobbit." },
+// ];
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node");
@@ -25,7 +26,9 @@ app.put("/livros/:id", (req, res) => {
 });
 
 app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+  livros.find((err, livros) => {
+    res.status(200).json(livros);
+  });
 });
 
 app.post("/livros", (req, res) => {
