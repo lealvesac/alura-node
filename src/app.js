6e73aedee50181d1b2eab5330dbf50bd9eb8392f
lealvesac <lealvesac@gmail.com>
2023-05-17 16:22:23 -0300

    finalização-crud

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dataBase from "./config/dbConnect.js";
-import livros from "./models/Livro.js";
 import routes from "./routes/index.js";
 
 dataBase.on("error", console.log.bind(console, "Erro de conexão!"));
@@ -13,35 +12,5 @@ const app = express();
 app.use(express.json());
 
 routes(app);
-
-app.put("/livros/:id", (req, res) => {
-  let index = buscalivros(req.params.id);
-  res.json(livros[index]);
-});
-
-
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("Livro cadastrado com sucesso!");
-});
-
-app.put("/livros/:id", (req,res) => {
-  let index = buscalivros(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.json(livros);
-});
-
-app.delete("/livros/:id", (req, res) => {
-  let { id } = req.params;
-  let index = buscalivros(id);
-  livros.splice(index, 1);
-  res.send(`Livro ${id} removido com sucesso.`);
-});
-
-function buscalivros(id) {
-  return livros.findIndex(livro => livro.id == id);
-}
-
-
 
 export default app;
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,13 +1,66 @@
 import livros from "../models/Livro.js";
 
 class LivroController {
-  
-    static listarLivros = (req, res) => {
+  static listarLivros = (req, res) => {
     livros.find((erro, livros) => {
       res.status(200).json(livros);
     });
-  }
-  
+  };
+
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id, (erro, livros) => {
+      if (erro) {
+        res.status(400).send({ message: `${erro.message} ID não encontrado.` });
+      } else {
+        res.status(200).send(livros);
+      }
+    });
+  };
+
+  static cadastrarLivro = (req, res) => {
+    let livro = new livros(req.body);
+
+    livro.save((erro) => {
+      if (erro) {
+        res
+          .status(500)
+          .send({ message: `${erro.message} - Falha ao cadastrar o Livro.` });
+      } else {
+        res.status(201).send(livro.toJSON());
+      }
+    });
+  };
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+    livros.findByIdAndUpdate(id, { $set: req.body }, (erro) => {
+      if (!erro) {
+        res
+          .status(200)
+          .send({ message: `O livro foi atualizado com sucesso.` });
+      } else {
+        res.status(500).send({ message: erro.message });
+      }
+    });
+  };
+
+  static excluirLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndDelete(id, (erro) => {
+      if (!erro) {
+        res
+          .status(200)
+          .send({ message: `O livro do ID ${id}, removido com sucesso` });
+      } else {
+        res
+          .status(500)
+          .send({ message: "Livro não encontrado para exclusão." });
+      }
+    });
+  };
 }
 
 export default LivroController;
